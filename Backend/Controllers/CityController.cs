9615f2512b86de6c0e7f7aca8b907d22fba4c201
Ankita Khokale <khokaleankita@kccemsr.edu.in>
2025-03-08 13:03:35 +0530

    Initial Commit

---
+++ Backend/Controllers/CityController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Neosoft_Ankita_Khokale_04March2025.Repository;
+using Neosoft_Ankita_Khokale_04March2025.Models;
+using System.Collections.Generic;
+
+namespace Neosoft_Ankita_Khokale_04March2025.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly CityRepository _repo;
+
+        public CityController(CityRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // Get all cities
+        [HttpGet]
+        public ActionResult<IEnumerable<City>> GetCities()
+        {
+            return Ok(_repo.GetCities());
+        }
+
+        // Get cities by state ID (for cascading dropdowns)
+        [HttpGet("byState/{stateId}")]
+        public ActionResult<IEnumerable<City>> GetCitiesByStateId(int stateId)
+        {
+            return Ok(_repo.GetCitiesByStateId(stateId));
+        }
+    }
+}
---
+++ Backend/Repository/StateRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Neosoft_Ankita_Khokale_04March2025.Data;
+using Neosoft_Ankita_Khokale_04March2025.Models;
+
+namespace Neosoft_Ankita_Khokale_04March2025.Repository
+{
+    public class StateRepository
+    {
+        private readonly DbHelper _dbHelper;
+
+        public StateRepository(DbHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+
+        public List<State> GetStates()
+        {
+            DataTable dt = _dbHelper.ExecuteStoredProcedure("stp_State_GetAll");
+            List<State> states = new List<State>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                states.Add(new State
+                {
+                    Row_Id = Convert.ToInt32(row["Row_Id"]),
+                    CountryId = Convert.ToInt32(row["CountryId"]),
+                    StateName = row["StateName"].ToString()
+                });
+            }
+            return states;
+        }
+
+        public List<State> GetStatesByCountryId(int countryId)
+        {
+            SqlParameter[] parameters = { new SqlParameter("@CountryId", countryId) };
+            DataTable dt = _dbHelper.ExecuteStoredProcedure("stp_State_GetByCountryId", parameters);
+            List<State> states = new List<State>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                states.Add(new State
+                {
+                    Row_Id = Convert.ToInt32(row["Row_Id"]),
+                    CountryId = Convert.ToInt32(row["CountryId"]),
+                    StateName = row["StateName"].ToString()
+                });
+            }
+            return states;
+        }
+    }
+}
